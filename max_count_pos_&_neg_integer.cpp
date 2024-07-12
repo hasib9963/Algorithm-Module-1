3a29c8dbf70c280d9a9f05e7896473367e9991f9
Hasib Mohammad Mahede Hasan <hasib9963@gmail.com>
2024-07-13 02:20:37 +0600

    Maximum Count and compare positive and negative integer in an array using a one pass algorithm. A Leetcode Problem.

---
+++ max_count_pos_&_neg_integer.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maximumCount(vector<int>& nums) {
+        int posCount = 0, negCount = 0;
+        
+        for (int num : nums) {
+            if (num > 0) {
+                posCount++;
+            } else if (num < 0) {
+                negCount++;
+            }
+        }
+        
+        return max(posCount, negCount);
+    }
+};
